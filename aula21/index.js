583f92d81fe9ef69e54146b93f85acc900f0012e
Ronald <ronaldmateus785@gmail.com>
2021-07-28 14:48:44 -0300

    add aula21

---
+++ aula21/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação
+>, maior que
+>=, maior que ou igual a 
+<, menor que
+<=, menor que ou igual a 
+==, igualdade (valor)
+===, igualdade estrita (valor e tipo)
+!=, diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+console.log(10 > 5); //true
+//Converte um tipo em outro
+console.log('5'== 5); //true
+console.log('5'=== 5); //false
+console.log('10'!= 10)//false
+console.log('10'!== 10)//true
